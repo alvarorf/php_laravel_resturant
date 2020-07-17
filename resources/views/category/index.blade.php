05101987e3e04d943f34ad1c72c34be0583119ac
alvaro <aaruizf@unal.edu.co>
2020-07-17 17:09:48 -0400

    Added table for food categories in index.blade.php

--- resources/views/category/index.blade.php
+++ resources/views/category/index.blade.php
@@ -8,9 +8,40 @@
                 <div class="card-header">All Categories</div>
 
                 <div class="card-body">
-                    @foreach($categories as $category)
-                        <p>{{$category->name}}</p>
-                    @endforeach
+                    <table class="table">
+                        <thead class="thead-dark">
+                            <tr>
+                                <th scope="col">#</th>
+                                <th scope="col">Name</th>
+                                <th scope="col">Edit</th>
+                                <th scope="col">Delete</th>
+                            </tr>
+                        </thead>
+                        <tbody>
+                            @if(count($categories)>0)
+                            @foreach($categories as $key=>$category)
+                            <tr>
+                                <th scope="row">{{$key+1}}</th>
+                                <td>{{$category->name}}</td>
+                                <td>
+                                    <a href="{{route('category.edit'
+                                    ,[$category->id])}}">
+                                        <button class="btn btn-outline-success">Edit</button>
+                                    </a></td>
+                                <td>
+                                    <a href="">
+                                        <button class="btn btn-outline-danger">Delete</button>
+                                    </a></td>
+                                </td>
+                            </tr>
+                            @endforeach
+
+                            @else
+                            <td>No category to display</td>
+                            @endif
+                            
+                        </tbody>
+                    </table>
                 </div>
             </div>
         </div>
