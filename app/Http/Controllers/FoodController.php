dcfe71c0abc59548a634384759dcff0a2b1c1f52
alvaro <aaruizf@unal.edu.co>
2020-07-21 23:14:44 -0400

    Added food creation form

--- app/Http/Controllers/FoodController.php
+++ app/Http/Controllers/FoodController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Food;
 class FoodController extends Controller
 {
     /**
@@ -23,7 +23,7 @@ class FoodController extends Controller
      */
     public function create()
     {
-        //
+        return view('food.create');
     }
 
     /**
